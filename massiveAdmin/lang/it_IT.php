e675cdb8a5b39c2e637aa9764a8dae01db862b9a
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2024-01-17 20:11:02 +0100

    5.0

---
+++ massiveAdmin/lang/it_IT.php
@@ -0,0 +1,153 @@
+<?php
+
+$i18n = [
+	'MAITENANCETITLE' 				=> 'Opzioni modalità di manutenzione',
+	'MAINTENANCE_ON' 				=> 'Modalità di manutenzione attivata?',
+	'CONTENT_MAINTENANCE_MODE' 		=> 'Contenuto della modalità di manutenzione:',
+	'TURNONBOOTSTRAPGRID' 			=> 'Attivare Bootstrap-Grid 4.0?',
+	'TURNONBOOTSTRAPGRIDONTHEME' 	=> 'Aggiungere CSS Bootstrap-Grid al tema front-end?',
+	'YES' 							=> 'Sì',
+	'NO' 							=> 'No',
+	'SAVEOPTION' 					=> 'Salva modifiche',
+	'SUPPORTO' 						=> 'Se vuoi sostenere il mio lavoro tramite PayPal :) Grazie! ',
+	'UPLOADFILE' 					=> 'Carica file',
+	'ANOTHERPAGE' 					=> '<strong>Modifiche salvate!</strong> Vai a un\'altra pagina:)',
+	'EDITCOMPONENTS' 				=> 'Modifica componenti',
+	'MASSIVEADMINSETTINGSTITLE' 	=> 'Impostazioni di amministrazione massiva',
+	'DROPFILES' 					=> 'Trascina qui i file da caricare',
+	'BOOTSTRAPTITLE' 				=> 'Opzioni griglia Bootstrap',
+
+	//options on file edit
+	'COPYFILE' 						=> 'Copia file',
+	'RENAMEFILE' 					=> 'Rinomina/Cambia directory',
+	'DOWNLOADFILE' 					=> 'Scarica',
+	'INFOCOPY' 						=> 'Il tuo file copiato si trova in',
+	'INFOERROR' 					=> 'Un file con questo nome esiste già in questa directory',
+	'FILENOW' 						=> 'Il tuo file si trova in',
+
+	//options on Create New User
+	'CREATENEWUSER' 				=> 'Crea nuovo utente',
+	'USERNAMECREATE' 				=> 'Nome utente:',
+	'PASSWORDCREATE' 				=> 'Password:',
+	'EMAILCREATE' 					=> 'E-mail nuovo utente:',
+	'LANGCREATE' 					=> 'Lingua utente:',
+
+	//options hidden section
+	'HIDEMENUTITLE' 				=> 'Gestione utenti e sezioni',
+	'SHOW' 							=> 'Mostra',
+	'HIDE' 							=> 'Nascondi',
+	'HIDSECTIONTITLE' 				=> 'Quali sezioni vuoi nascondere?',
+	'HIDEPAGES' 					=> 'Nascondere la sezione "<strong><i class="uil uil-desktop"></i>Pagine</strong>"?',
+	'HIDEFILES' 					=> 'Nascondere la sezione "<strong><i class="uil uil-file"></i>File</strong>"?',
+	'HIDETHEMES' 					=> 'Nascondere la sezione "<strong><i class="uil uil-paint-tool"></i> Temi</strong>"?',
+	'HIDEBACKUP' 					=> 'Nascondere la sezione "<strong><i class="uil uil-save"></i>Backup</strong>"?',
+	'HIDEPLUGIN' 					=> 'Nascondere la sezione "<strong><i class="uil uil-plug"></i>Plugin</strong>"?',
+	'HIDESUPPORT' 					=> 'Nascondere la sezione "<strong><i class="uil uil-life-ring"></i> Assistenza</strong>"?',
+	'HIDESETTINGS' 					=> 'Nascondere la sezione "<strong><i class="uil uil-setting"></i> Impostazioni MassiveAdmin</strong>"?',
+	'HIDEI18NGALLERY' 				=> 'Nascondere la sezione "<strong><i class="uil uil-images"></i> galleria i18n</strong>"? (se esiste)',
+	'HIDEUSERS' 					=> 'Nascondi le sezioni per i seguenti utenti:',
+
+	//user manager
+	'USERMANAGER' 					=> 'Gestisci utenti',
+
+	//user help settings
+	'USERHELPTITLE' 				=> 'Impostazioni guida utente',
+	'TURNON' 						=> 'Accendere?',
+	'HELP' 							=> 'Aiuto',
+	'USERHELP' 						=> 'Aiuto utente',
+
+	//menu external
+	'MENUEXTERNAL' 					=> 'Gestione collegamenti menu personalizzati',
+	'LINKNAME' 						=> 'Nome collegamento:',
+	'LINKURL' 						=> 'URL collegamento:',
+	'LINKICON' 						=> 'Icona collegamento',
+	'NEWWINDOW' 					=> 'Apri il link in una nuova finestra?',
+	'MOREICONS' 					=> '(Trova altre icone qui.):',
+	'ADDLINK' 						=> 'Aggiungi nuovo collegamento',
+
+	//own footer option
+	'OWNFOOTERTITLE' 				=> 'Opzioni etichetta bianca',
+	'OWNFOOTERNAME' 				=> 'Nome piè di pagina personalizzato:',
+	'OWNFOOTERICON' 				=> 'Icona piè di pagina personalizzata (altezza massima 256px | .jpg, .jpeg, .png, .gif)',
+	'OWNLOGO' 						=> 'Logo personalizzato nella pagina di accesso?',
+	'OWNFOOTERLINK' 				=> 'Link a piè di pagina personalizzato:',
+	'OWNFOOTERHEADER' 				=> 'CSS/Script intestazione personalizzata:',
+	'OWNFOOTERFOOTER' 				=> 'Script piè di pagina personalizzati:',
+
+	//owncolor
+	'CHANGETITLECOLOR' 				=> 'Cambia colori CMS',
+	'MAINCOLOR' 					=> 'Colore principale:',
+	'BGCOLOR' 						=> 'Colore sfondo:',
+	'CHANGENAME' 					=> 'cambia il nickname in qualcosa di diverso dal nickname dell\'amministratore',
+
+	//snippet
+	'SNIPPET' 						=> 'Snippet',
+	'ADDSNIPPET' 					=> 'Aggiungi snippet',
+	'EDITSNIPPET' 					=> 'Modifica snippet',
+	'TITLESNIPPET' 					=> 'Titolo (senza spazi)',
+	'SUBMITSNIPPET' 				=> 'Salva frammenti',
+
+	//migrate
+	'MIGRATETITLE' 					=> 'Migrazione sito e SSL',
+	'MIGRATEDOMAIN' 				=> 'Migrazione dominio',
+	'OLDDOMAIN' 					=> 'Indirizzo vecchio sito web',
+	'NEWDOMAIN' 					=> 'Nuovo indirizzo sito web',
+	'REPLACEDOMAIN' 				=> 'Sostituisci URL',
+	'FORCESSL' 						=> 'Forza SSL',
+	'TURNONSSL' 					=> 'Forzare SSL nell\'URL?',
+	'SAVESSL' 						=> 'Salva impostazioni SSL',
+
+	//plugin downloader
+	'PLUGINDOWNLOADER' 				=> 'Scarica plugin CE',
+	'DOWNLOADRBASED' 				=> 'Basato sul repository dei plugin Get Simple CMS CE.',
+	'SEARCHPLUGIN' 					=> 'Plugin di ricerca...',
+	'DOWNLOAD' 						=> 'Scarica',
+
+	//login options
+	'LOGINOPTIONS' 					=> 'Opzioni di accesso',
+	'TURNONSHOWPASSWORDTITLE' 		=> 'Pulsante "Mostra password"',
+	'SHOWPASSWORDQUESTION' 			=> 'Attivare il pulsante "Mostra password" quando accedi?',
+	'REMOVEFORGETPASSWORDTITLE' 	=> 'Rimuovere "Password dimenticata?"',
+	'SAVESETTINGS' 					=> 'Salva impostazioni',
+
+	//GSCONFIG
+	'GSCONFIGTITLE' 				=> 'Modifica impostazioni GSCONFIG',
+	'GSCONFIGSAVE' 					=> 'Salva le impostazioni GSCONFIG',
+
+	'UNISTALLER' 					=> 'Disinstalla plugin',
+	'UNISTALLQUESTION' 				=> 'Vuoi disinstallare',
+
+	//mToper
+	"TAB_PAGES" 					=> 'Pagine',
+	"TAB_FILES" 					=> 'File',
+	"TAB_THEME" 					=> 'Tema',
+	"TAB_BACKUPS" 					=> 'Backup',
+	"PLUGINS_NAV"					=> 'Plugin',
+	"TAB_SETTINGS" 					=> 'Impostazioni',
+	"TAB_LOGOUT" 					=> 'Esci',
+
+	//makeFileInTheme
+	"CREATENEWFILE" 				=> 'Crea nuovo file',
+	"FILECREATED" 					=> 'File creato!',
+	
+	//frontend settings
+
+
+	//new on 4.0
+
+	//admin settings
+
+	'FRONTENDTITLE' 				=> 'Impostazioni front-end',
+	'TURNONMTOPER' 					=> 'Attivare la barra frontend?',
+	'STYLEINFO' 					=> 'Stile per barra laterale FrontEnd',
+	'ADMINTHEMESELECTOR' 			=> 'Selettore tema amministratore',
+
+	//manage users
+
+	'EMAIL' 						=> 'e-mail:',
+	'PASSWORD' 						=> 'Password:',
+	'LANG' 							=> 'Lingua:',
+	'CHANGEPLACEHOLDER' 			=> 'cambia solo se vuoi cambiare'
+
+
+];
